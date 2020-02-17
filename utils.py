e366f0f495b810df6f5bd6e0d04415acf600fe1d
mingtian <me@tomo.wiki>
2020-02-17 10:41:48 +0000

    add online step

--- utils.py
+++ utils.py
@@ -4,6 +4,10 @@ def save_data(opt,i,train_list,test_list):
     np.save(train_file,np.asarray(train_list))
     test_file=opt['file']+'test_accuracy_list'
     np.save(test_file,np.asarray(test_list))
-    
+
+def save_accuracy_vs_data(opt,test_accuracy_vs_data_list):
+
+    target_file=opt['file']+'test_accuracy_vs_data'
+    np.save(target_file,np.asarray(test_accuracy_vs_data_list))
 
 
